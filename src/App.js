a52858cf39c057a56077638b3a8975f9205a43a4
jhomble <jhomble7@gmail.com>
2019-03-18 00:52:01 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import DomTreeInputComponent from './components/dom-tree-input/dom-tree-input.component.js';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolder, faFile, faPlusSquare, faMinusSquare } from '@fortawesome/free-regular-svg-icons'
 
 library.add(faFolder, faFile, faPlusSquare, faMinusSquare)
